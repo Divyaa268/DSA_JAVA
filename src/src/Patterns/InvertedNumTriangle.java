8bc2084e2f2aac8b0183b477040babc7b193c5c6
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-07-08 14:31:59 -0400

    Inverted Triangle with Numbers pattern.

---
+++ src/src/Patterns/InvertedNumTriangle.java
@@ -0,0 +1,16 @@
+package Patterns;
+
+public class InvertedNumTriangle {
+
+    public static void main(String[] args) {
+
+        for(int r=5;r>=1;r--)
+        {
+            for(int c=1; c<=r;c++)
+            {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+}
