3e01f7547e6c16400829456f137a199bfdad7969
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-07-08 14:26:28 -0400

    Triangle with Numbers pattern.

---
+++ src/src/Patterns/TriangleWithNum.java
@@ -0,0 +1,22 @@
+package Patterns;
+
+//1
+//12
+//123
+//1234
+//12345
+
+public class TriangleWithNum {
+
+    public static void main(String[] args) {
+
+        for(int r=1;r<=5;r++)
+        {
+            for(int c=1;c<=r;c++)
+            {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+}
