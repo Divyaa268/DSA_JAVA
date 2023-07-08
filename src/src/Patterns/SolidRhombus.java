9b3c13403b0958234a1f9a96c95abbb59f207d95
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-07-08 17:12:03 -0400

    Solid Rhombus pattern.

---
+++ src/src/Patterns/SolidRhombus.java
@@ -0,0 +1,22 @@
+package Patterns;
+
+public class SolidRhombus {
+
+    public static void main(String[] args) {
+
+        int n= 5;
+
+        for(int i=n;i>=1;i--)
+        {
+            for(int s = 1;s<=i-1;s++)
+            {
+                System.out.print("-");
+            }
+            for (int j=1;j<=n;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
