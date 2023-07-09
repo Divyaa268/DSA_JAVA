9da5e849e330162a291517299a9ed2a65fc25695
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-07-09 11:39:34 -0400

    Linear Search.

---
+++ src/src/Arrays/LinearSearch.java
@@ -0,0 +1,41 @@
+package Arrays;
+
+// Time Complexity - O(n)
+
+public class LinearSearch {
+
+    public static int linearSearch(int[] a, int n)
+    {
+        int l = a.length;
+        int pos = 0;
+
+        for(int i=0;i<l;i++)
+        {
+            if(a[i] == n)
+            {
+                pos = i;
+                return pos;
+            }
+        }
+        return -1;
+
+    }
+
+
+    public static void main(String[] args) {
+
+        int a[] = {2, 4, 7, 8, 9, 3};
+        int n = 4;
+
+        int p = linearSearch(a, n);
+
+        if (p == -1) {
+            System.out.println("Number not found!");
+        } else {
+
+            System.out.println("Number found at index: " + p);
+        }
+    }
+
+
+}
