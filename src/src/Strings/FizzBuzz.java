cce95a4f7f4be02932f322b5784b651f9fbb9655
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-07-17 18:58:17 -0400

    Fizz Buzz

---
+++ src/src/Strings/FizzBuzz.java
@@ -0,0 +1,42 @@
+package Strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+
+    public static List<String> fizzBuzz(int n) {
+
+        List<String> s = new ArrayList<>();
+
+        for(int i=1;i<=n;i++)
+        {
+            if((i%3 !=0 && i%5 != 0) && i%3 !=0 && i%5 !=0)
+            {
+                s.add(String.valueOf(i));
+            }
+            else if( i%3 == 0 && i%5 != 0 )
+            {
+                s.add("Fizz");
+            }
+            else if( i%5 == 0  && i%3 != 0)
+            {
+                s.add("Buzz");
+            }
+            else
+            {
+                s.add("FizzBuzz");
+            }
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        List<String> s = fizzBuzz(n);
+        System.out.println(s);
+
+    }
+}
