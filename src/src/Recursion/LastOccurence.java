ff4febb3183d3dc12f668ffc2a2c0732becba90e
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-07-19 19:34:11 -0400

    last occurence of element

---
+++ src/src/Recursion/LastOccurence.java
@@ -0,0 +1,25 @@
+package Recursion;
+
+public class LastOccurence {
+
+    public static int lastOccur(int a[], int i, int n)
+
+    {
+        if(i < 0)
+        {
+            return -1;
+        }
+        if(a[i] == n )
+        {
+            return i;
+        }
+        return lastOccur(a, i-1,n);
+    }
+
+    public static void main(String[] args) {
+        int a[] = {1,2,5,7,8,4,2};
+        int n = 2;
+        int i = lastOccur(a,a.length-1, n);
+        System.out.println(i);
+    }
+}
