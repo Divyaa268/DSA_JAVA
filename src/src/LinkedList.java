dfcde10f7b1a9312bf5f2c78d2e721e2f373693c
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-07-28 00:03:47 -0400

    Linked List ADD FIRST

--- src/src/LinkedList.java
+++ src/src/LinkedList.java
@@ -11,4 +11,32 @@ public class LinkedList {
             this.next = null;
         }
     }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data)  // TC = O(1)
+    {
+        // create a new node
+        Node newNode = new Node(data);
+
+        // if LL is empty
+        if(head == null)
+        {
+            head = tail = newNode;
+        }
+
+        // point newNode's next to head
+        newNode.next = head;
+
+        // make newNode as the head
+        head = newNode;
+    }
+
+    public static void main(String args[])
+    {
+        LinkedList ll = new LinkedList();
+        ll.addFirst(1);
+        ll.addFirst(2);
+    }
 }
