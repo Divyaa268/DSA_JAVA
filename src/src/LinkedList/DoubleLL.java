a787a3dc32430f52fb9484e64d37303b6d288b51
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-08-03 01:31:20 -0400

    Double LinkedList addFirst

--- src/src/LinkedList/DoubleLL.java
+++ src/src/LinkedList/DoubleLL.java
@@ -19,8 +19,40 @@ public class DoubleLL {
     public static Node tail;
     public static int size;
 
+    // Add
+    public void addFirst(int data)
+    {
+        Node newNode = new Node(data);
+        if(head == null)
+        {
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    public void printDLL()
+    {
+        Node temp = head;
+
+        while (temp != null)
+        {
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
     public static void main(String args[])
     {
+        DoubleLL dll = new DoubleLL();
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
 
+        dll.printDLL();
     }
 }
