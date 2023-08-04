09a7c6a6a580b74f8f3892ba9785381ebf05ec5c
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-08-04 00:42:56 -0400

    Push at bottom using Stack

---
+++ src/src/Stack/PushAtBottom.java
@@ -0,0 +1,33 @@
+package Stack;
+
+import java.util.Stack;
+
+public class PushAtBottom {
+
+    public static void pushBottom(Stack<Integer> s, int element)
+    {
+        if(s.isEmpty())
+        {
+            s.push(element);
+            return;
+        }
+        int top = s.pop();
+        pushBottom(s, element);
+        s.push(top);
+    }
+
+
+    public static void main(String args[]) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        pushBottom(s, 4);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
