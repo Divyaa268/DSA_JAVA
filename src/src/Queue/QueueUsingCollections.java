a1515e0e39ed7a457dc508065d29b3b5fa040d7f
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-08-15 20:24:44 -0400

    Queue using Javva Collections Framework

--- src/src/Queue/QueueUsingCollections.java
+++ src/src/Queue/QueueUsingCollections.java
@@ -5,6 +5,7 @@ public class QueueUsingCollections {
 
     public static void main(String[] args) {  // TC - O(1) in all three operations
         Queue<Integer> q = new LinkedList<>(); // we can also make queue using ArrayDeque
+        Queue<Integer> q1 = new ArrayDeque<>();
         q.add(1);
         q.add(2);
         q.add(3);
