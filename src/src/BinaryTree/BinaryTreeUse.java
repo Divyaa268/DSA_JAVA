6a988b1be4802c71dec934cd498d76f463de5872
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-08-17 15:10:42 -0400

    Inorder Traversal

--- src/src/BinaryTree/BinaryTreeUse.java
+++ src/src/BinaryTree/BinaryTreeUse.java
@@ -56,6 +56,18 @@ public class BinaryTreeUse {
             System.out.print(root.data + " ");
         }
 
+        public static void inorder(Node root)
+        {
+            if(root == null)
+            {
+                return;
+            }
+
+            inorder(root.left);
+            System.out.println(root.data + " ");
+            inorder(root.right);
+        }
+
         public static void main(String[] args) {
             int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
             BinaryTree tree = new BinaryTree();
@@ -68,6 +80,9 @@ public class BinaryTreeUse {
 
             System.out.println("Postorder");
             tree.postorder(root);
+
+            System.out.println("Inorder");
+            tree.inorder(root);
 
 
         }
