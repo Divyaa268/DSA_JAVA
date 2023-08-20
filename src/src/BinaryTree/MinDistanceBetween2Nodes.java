5348d6014f242cc9e7380ecb4bfc9122865af633
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-08-20 18:32:48 -0400

    Minimum Distance between two nodes.

---
+++ src/src/BinaryTree/MinDistanceBetween2Nodes.java
@@ -0,0 +1,93 @@
+package BinaryTree;
+
+public class MinDistanceBetween2Nodes {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node lca2(Node root, int n1, int n2)
+    {
+        if(root == null || root.data == n1 || root.data == n2)
+        {
+            return root;
+        }
+
+        Node leftLCA = lca2(root.left, n1, n2);
+        Node rightLCA = lca2(root.right, n1, n2);
+
+        if(leftLCA == null)
+        {
+            return rightLCA;
+        }
+
+        if(rightLCA == null)
+        {
+            return leftLCA;
+        }
+        return root;
+    }
+
+    public static int lcaDist(Node root, int n)
+    {
+        if(root == null)
+        {
+            return -1;
+        }
+        if(root.data == n)
+        {
+            return 0;
+        }
+
+        int leftDist = lcaDist(root.left, n);
+        int righDist = lcaDist(root.right, n);
+
+        if(leftDist == -1 && righDist == -1)
+        {
+            return -1;
+        }
+        else if (leftDist == -1)
+        {
+            return righDist + 1;
+        }
+        else
+        {
+            return leftDist + 1;
+        }
+    }
+
+    public static int minDist(Node root, int n1, int n2)
+    {
+
+        // Find LCA for n1 and n2
+        Node lca = lca2(root, n1, n2);
+
+        // Find the distance between LCA and n1 and LCA and n2
+        int dist1 = lcaDist(lca, n1);
+        int dist2 = lcaDist(lca, n2);
+
+        return dist1 + dist2;
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println("Minimum distance between 2 nodes is - " + minDist(root, 4,6));
+
+    }
+}
