c7f6863df8a543b899f0102d6b634d66c415d61d
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-08-24 01:28:49 -0400

    Create an Info class.

--- src/src/BinarySearchTree/BST.java
+++ src/src/BinarySearchTree/BST.java
@@ -18,6 +18,23 @@ public class BST {
         }
     }
 
+    static class Info
+    {
+        boolean isBST;
+        int size;
+        int min;
+        int max;
+
+        public Info(boolean isBST, int size, int min, int max)
+        {
+            this.isBST = isBST;
+            this.size = size;
+            this.min = min;
+            this.max = max;
+        }
+
+    }
+
     public static void inorder(Node root)
     {
         if(root == null)
