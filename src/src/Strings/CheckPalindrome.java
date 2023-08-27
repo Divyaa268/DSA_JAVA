05df6296a7997039728130ce99b638f7ffa244c6
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-08-27 13:33:03 -0400

    Check palindrome

--- src/src/Strings/CheckPalindrome.java
+++ src/src/Strings/CheckPalindrome.java
@@ -18,11 +18,34 @@ public class CheckPalindrome
         return true;
     }
 
+    public static boolean palin(String s) {
+        int l = s.length();
+
+        String str = "";
+
+        for (int i = l-1; i >= 0; i--) {
+            str = str + s.charAt(i);
+        }
+        System.out.println(str);
+        if (s.equals(str))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 
     public static void main(String[] args) {
         String s = "madam";
 
         boolean res = checkPalin(s);
         System.out.println(res);
+
+
+        boolean r = palin(s);
+        System.out.println(r);
     }
 }
