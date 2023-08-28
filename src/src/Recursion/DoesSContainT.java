80b5f38616e0eee64d3cb45d3c67b9848d315e7e
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-08-28 03:11:58 -0400

    Does String S contain String T

---
+++ src/src/Recursion/DoesSContainT.java
@@ -0,0 +1,75 @@
+package Recursion;
+
+public class DoesSContainT {
+
+    public static boolean checkSeq(String a, String b) {
+
+
+
+        int m=a.length();
+
+        int n=b.length();
+
+        return checkSequence(a,b,m,n);
+
+    }
+
+
+
+    public static boolean checkSequence(String a, String b, int m, int n)
+
+    {
+
+        if(m==0)
+
+        {
+
+            return false;
+
+        }
+
+
+
+        if(n==0)
+
+        {
+
+            return true;
+
+
+
+        }
+
+        // If last characters of two strings are matching
+
+        if(a.charAt(m-1)==b.charAt(n-1))
+
+        {
+
+
+
+            return checkSequence(a,b, m-1, n-1);
+
+        }
+
+        // If last characters of two strings are not matching
+
+        return  checkSequence(a,b, m,n-1);
+
+    }
+
+    public static void main(String[] args) {
+
+
+
+        String a ="aackkodliwnkgd";
+
+        String b = "coding";
+
+        boolean r=checkSeq(a,b);
+
+        System.out.println(r);
+
+    }
+
+}
