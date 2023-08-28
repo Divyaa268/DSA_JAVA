3cfe82693e1599332780a026f85aebc12a1ef865
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-08-28 01:23:39 -0400

    Check if strings are isomorphic

--- src/src/Strings/IsomorphicStrings.java
+++ src/src/Strings/IsomorphicStrings.java
@@ -12,17 +12,26 @@ public class IsomorphicStrings {
         int [] a2 = new int[256];
 
 
+
         for(int i=0;i<s.length();i++)
         {
             char a = s.charAt(i);
             char b = t.charAt(i);
 
+            System.out.println("f" + a1[a]);
+            System.out.println("f" + a2[b]);
+
             if(a1[a] != a2[b])
             {
                 return false;
             }
+
             a1[a] = i+1;
             a2[b] = i+1;
+
+            System.out.println("s" + a1[a]);
+            System.out.println("s" + a2[b]);
+
         }
         return true;
     }
