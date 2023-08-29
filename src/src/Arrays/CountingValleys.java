3ed2d559cee5ac8e3cfd12527446a0589d173c62
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-08-28 21:47:15 -0400

    Count no. of valleys traversed.

---
+++ src/src/Arrays/CountingValleys.java
@@ -0,0 +1,40 @@
+package Arrays;
+
+public class CountingValleys {
+
+    public static int countvalley(int steps, String path)
+    {
+        int l = path.length();
+        int level = 0;
+        int valley = 0;
+
+        for (int i=0;i<l;i++)
+        {
+            if(path.charAt(i) == 'U')
+            {
+                level++;
+
+                if(level == 0)
+                {
+                    valley++;
+                }
+            }
+            else
+            {
+                level--;
+            }
+        }
+        return valley;
+
+    }
+
+    public static void main(String[] args) {
+
+        String path = "UDDDUDUU";
+        int steps = 8;
+
+        int valleys = countvalley(steps, path);
+        System.out.println("No. of valleys traversed : " + valleys);
+
+    }
+}
