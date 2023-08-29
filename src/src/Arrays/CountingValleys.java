9ee9a5b7599cf497531f7a072b05490f2070a8c3
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-08-28 21:47:51 -0400

    Count no. of valleys traversed.

--- src/src/Arrays/CountingValleys.java
+++ src/src/Arrays/CountingValleys.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+// https://www.hackerrank.com/challenges/counting-valleys/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
 public class CountingValleys {
 
     public static int countvalley(int steps, String path)
