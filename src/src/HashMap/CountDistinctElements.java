e32966a95e2f1a86763ab7400ddfa1fef660cd08
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-09-02 21:43:57 -0400

    Count distinct elements.

---
+++ src/src/HashMap/CountDistinctElements.java
@@ -0,0 +1,20 @@
+package HashMap;
+
+import java.util.HashSet;
+
+public class CountDistinctElements {
+
+    public static void main(String[] args) {
+
+        int nums[] = {1,2,3,4,2,1,5,6,7,8,4};
+
+        HashSet<Integer> hs = new HashSet<>();
+
+        for (int i=0;i<nums.length;i++)
+        {
+            hs.add(nums[i]);
+        }
+
+        System.out.println("Count is : "+ hs.size());
+    }
+}
