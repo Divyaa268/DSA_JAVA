25b134c062275b0cefa5174779d4c12eb740f8a7
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-09-05 14:25:20 -0400

    Max difference 1

---
+++ src/src/Arrays/MaxDifference1.java
@@ -0,0 +1,37 @@
+package Arrays;
+
+public class MaxDifference1 {
+
+    public static void main(String[] args) {
+
+        int a[] = {3,9,8,4,1,2,10,7};
+        int ans = maxDiff(a);
+        System.out.println(ans);
+    }
+
+    public static int maxDiff(int a[])
+    {
+        int min  = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0;i<a.length;i++)
+        {
+            if(max< a[i])
+            {
+                max = a[i];
+            }
+        }
+
+        for(int i=0;i<a.length;i++)
+        {
+            if(min> a[i])
+            {
+                min = a[i];
+            }
+        }
+
+        return max - min;
+
+    }
+
+}
