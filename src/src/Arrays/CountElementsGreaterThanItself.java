1c58ae0b313b64bf372860dd9ab3f9b59241d267
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-09-05 03:51:45 -0400

    Count elements having at least one element greater than itself

---
+++ src/src/Arrays/CountElementsGreaterThanItself.java
@@ -0,0 +1,36 @@
+package Arrays;
+
+public class CountElementsGreaterThanItself {
+
+    public static void main(String[] args) {
+
+        int a[] = {-1, -3,7,9,3,4};
+
+        int ans = greaterEle(a);
+        System.out.println(ans);
+    }
+
+    public static int greaterEle(int a[])
+    {
+        int count = 0;
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0;i<a.length;i++)
+        {
+            if(a[i] > max)
+            {
+                max = a[i];
+            }
+        }
+
+        for (int i =0;i<a.length;i++)
+        {
+            if(a[i] == max)
+            {
+                count++;
+            }
+        }
+        int ans = a.length - count;
+        return ans;
+    }
+}
