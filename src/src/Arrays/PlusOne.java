b21557bcf99ae85172b25fdb8e96f640b945a5b5
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-09-09 20:39:05 -0400

    Plus one

---
+++ src/src/Arrays/PlusOne.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+public class PlusOne {
+
+    public static void main(String[] args) {
+
+        int a[] = {5, 6};
+        int res[] = plusOne(a);
+
+        for (int i = 0; i < res.length; i++)
+        {
+            System.out.print(res[i] + " ");
+        }
+    }
+    public static int[] plusOne(int[] a) {
+
+        int l = a.length;
+
+        for(int i=l-1;i>=0;i--)
+        {
+            if(a[i] < 9)
+            {
+                a[i]++;
+                return a;
+            }
+            a[i] = 0;
+        }
+
+        a = new int[l+1];
+        a[0] = 1;
+        return a;
+
+    }
+}
