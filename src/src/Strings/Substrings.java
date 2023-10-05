e12c014054a52490aa173faf068a3fc6b40f95c0
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-10-05 17:30:29 -0400

    substrings

---
+++ src/src/Strings/Substrings.java
@@ -0,0 +1,27 @@
+package Strings;
+
+import java.util.*;
+
+public class Substrings {
+
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        String st = s.nextLine();
+        printSubstrings(st);
+    }
+
+
+    public static void printSubstrings(String str)
+    {
+        int l=str.length();
+        for(int i=0;i<l;i++)
+        {
+            for(int j=i;j<l;j++)
+            {
+                System.out.println(str.substring(i,j+1));
+            }
+        }
+//        System.out.println(str.substring(l-1));
+    }
+}
