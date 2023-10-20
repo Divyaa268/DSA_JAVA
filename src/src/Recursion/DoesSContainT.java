9310147c48204653718b2c262368e2441d0a75f2
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-10-20 04:41:45 -0400

    Does s contain t

--- src/src/Recursion/DoesSContainT.java
+++ src/src/Recursion/DoesSContainT.java
@@ -19,57 +19,31 @@ public class DoesSContainT {
     public static boolean checkSequence(String a, String b, int m, int n)
 
     {
-
         if(m==0)
-
         {
-
             return false;
-
         }
 
-
-
         if(n==0)
-
         {
-
             return true;
-
-
-
         }
 
         // If last characters of two strings are matching
-
         if(a.charAt(m-1)==b.charAt(n-1))
-
         {
-
-
-
             return checkSequence(a,b, m-1, n-1);
-
         }
 
         // If last characters of two strings are not matching
-
         return  checkSequence(a,b, m,n-1);
-
     }
 
     public static void main(String[] args) {
-
-
-
         String a ="aackkodliwnkgd";
-
         String b = "coding";
 
         boolean r=checkSeq(a,b);
-
         System.out.println(r);
-
     }
-
 }
