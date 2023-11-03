494db762f20f3d5e8de01300aa11a20abf8fde72
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-11-03 03:51:49 -0400

    pdf viewer

--- src/src/Arrays/PDFViewer.java
+++ src/src/Arrays/PDFViewer.java
@@ -44,6 +44,55 @@ public class PDFViewer {
 
     }
 
+
+//    import java.io.*;
+//import java.math.*;
+//import java.security.*;
+//import java.text.*;
+//import java.util.*;
+//import java.util.concurrent.*;
+//import java.util.regex.*;
+//import java.lang.*;
+//
+//    class Result {
+//
+//        /*
+//         * Complete the 'designerPdfViewer' function below.
+//         *
+//         * The function is expected to return an INTEGER.
+//         * The function accepts following parameters:
+//         *  1. INTEGER_ARRAY h
+//         *  2. STRING word
+//         */
+//
+//        public static int designerPdfViewer(List<Integer> h, String word) {
+//            // Write your code here
+//
+//            int length = word.length();
+//
+//            // Index of the Letters of the Words are 'character' - 'a'
+//
+//            int maxHeight = -1;
+//
+//            for (char letter:
+//                    word.toCharArray()) {
+//                int IndexValue = letter - 'a';
+//                System.out.println("Index "+IndexValue);
+//
+//                int heightValue = h.get(IndexValue);
+//                System.out.println("Height Value At Index is "+heightValue);
+//
+//                maxHeight = Math.max(maxHeight, heightValue);
+//            }
+//
+//
+//
+//
+//            return maxHeight * length;
+//        }
+//
+//    }
+
     public static void main(String[] args) {
 
         List<Integer> h = List.of(1,3,1,3,1,4,1,3,2,5,5,5,5,1,1,5,5,1,5,2,5,5,5,5,5,5);
