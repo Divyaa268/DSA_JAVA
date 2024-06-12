bc507e282e2ed899e6c9433cd7a521f05be3b8cd
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-06-12 14:37:02 -0400

    Basic calculator

--- DSA_JAVA-main/src/src/PracticeDSA/Calculator.java
+++ DSA_JAVA-main/src/src/PracticeDSA/Calculator.java
@@ -12,7 +12,7 @@ public class Calculator {
         System.out.print("Enter first number: ");
         double num1 = scanner.nextDouble();
 
-        System.out.print("Enter second number: ");
+        System.out.print("Enter second number");
         double num2 = scanner.nextDouble();
 
         System.out.print("Enter an operator (+, -, *, /): ");
