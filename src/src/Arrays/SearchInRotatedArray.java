853a8aaa0d734def365870f0c9d066d9960de613
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-06-20 01:11:52 -0400

    search in rotated array 33 LC

--- src/src/Arrays/SearchInRotatedArray.java
+++ src/src/Arrays/SearchInRotatedArray.java
@@ -15,10 +15,54 @@ public class SearchInRotatedArray {
         return -1;
     }
 
+    public static int searchoOptimized(int[] nums, int target) {
+    int left = 0;
+    int right = nums.length -1;
+
+        while(left <= right)
+    {
+        int mid = (left + right)/2;
+
+        if(nums[mid] == target)
+        {
+            return mid;
+        }
+
+        // check which half is sorted
+        if(nums[left] <= nums[mid])
+        {
+            // find target in left half
+            if(nums[left] <= target && target < nums[mid])
+            {
+                right = mid-1;
+            }
+            else
+            {
+                left = mid +1;
+            }
+        }
+        else
+        {
+            // right half is sorted
+            if(nums[mid] < target && target <= nums[right])
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid-1;
+            }
+        }
+    }
+        return -1;
+}
+
+
     public static void main(String args[]) {
 //        int a[] = {1,2,3,1};
         int a[] = {1, 2, 1, 3, 5, 6, 4};
 
         System.out.println(search(a, 5));
+        System.out.println(searchoOptimized(a, 5));
     }
 }
