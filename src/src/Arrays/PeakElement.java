a25b813f6574c849721cbbc59c767ca14a6e329d
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-06-19 23:54:42 -0400

    peak element 162 optimized

--- src/src/Arrays/PeakElement.java
+++ src/src/Arrays/PeakElement.java
@@ -16,11 +16,33 @@ public class PeakElement {
         return -1;
     }
 
+    public static int findPeakElementOptimzed(int[] nums) { // O(log n)
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                // Peak is in the left half including mid
+                right = mid;
+            } else {
+                // Peak is in the right half excluding mid
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+
     public static void main(String args[])
     {
 //        int a[] = {1,2,3,1};
         int a[] = {1,2,1,3,5,6,4};
 
         System.out.println(findPeakElement(a));
+        System.out.println(findPeakElementOptimzed(a));
     }
 }
