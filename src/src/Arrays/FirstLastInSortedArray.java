32451a689d24f9a670f1d3675ad5c0506e49a80b
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-06-22 21:56:13 -0400

    34. Find First and Last Position of Element in Sorted Array optimized

--- src/src/Arrays/FirstLastInSortedArray.java
+++ src/src/Arrays/FirstLastInSortedArray.java
@@ -18,6 +18,53 @@ public class FirstLastInSortedArray {
                 return new int[] {first, last};
             }
 
+    public int[] searchRangeOpt(int[] nums, int target) {
+
+        int[] result = new int[2];
+        result[0] = findFirst(nums, target);
+        result[1] = findLast(nums, target);
+        return result;
+    }
+
+    private int findFirst(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int index = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+            if (nums[mid] == target) {
+                index = mid;
+            }
+        }
+        return index;
+    }
+
+    private int findLast(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int index = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+            if (nums[mid] == target) {
+                index = mid;
+            }
+        }
+        return index;
+    }
+
+
     public static void main(String[] args) {
         int[] nums1 = {5,7,7,8,8,10};
         int t = 8;
