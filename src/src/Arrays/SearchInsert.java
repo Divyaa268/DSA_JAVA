97d6f75542ce80f6ee099caa946f71cdb8efa1d7
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-06-24 00:22:36 -0400

    35. Search Insert Position

---
+++ src/src/Arrays/SearchInsert.java
@@ -0,0 +1,36 @@
+package Arrays;
+
+public class SearchInsert {
+        public int searchInsert(int[] nums, int target) {
+            // Edge case: if array is empty, target should be inserted at index 0
+            if (nums.length == 0) {
+                return 0;
+            }
+
+            // Linear search to find the insertion index
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] >= target) {
+                    return i;
+                }
+            }
+
+            // If target is greater than all elements in nums, insert at the end
+            return nums.length;
+        }
+
+        public static void main(String[] args) {
+            SearchInsert solution = new SearchInsert();
+
+            int[] nums1 = {1, 3, 5, 6};
+            int target1 = 5;
+            System.out.println(solution.searchInsert(nums1, target1));  // Output: 2
+
+            int[] nums2 = {1, 3, 5, 6};
+            int target2 = 2;
+            System.out.println(solution.searchInsert(nums2, target2));  // Output: 1
+
+            int[] nums3 = {1, 3, 5, 6};
+            int target3 = 7;
+            System.out.println(solution.searchInsert(nums3, target3));  // Output: 4
+        }
+    }
