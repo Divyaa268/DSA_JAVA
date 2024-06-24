06a0844941454a42bc2bc0154711801a240314a0
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-06-24 18:51:09 -0400

    pair sum o(n^2) tc

--- src/src/PracticeDSA/PairSum.java
+++ src/src/PracticeDSA/PairSum.java
@@ -26,17 +26,47 @@ public class PairSum {
             return new int[0];
         }
 
+    public static int[] twoSum(int[] nums, int target) {
+
+            int l = nums.length;
+
+            for(int i=0;i<l;i++)
+            {
+                for(int j=i;j<l;j++)
+                {
+                    if(nums[i] + nums[j] == target)
+                    {
+                        return new int[] {i,j};
+                    }
+                }
+            }
+            return new int[] {};
+    }
+
         public static void main(String[] args) {
             int[] nums = {2, 7, 11, 15};
             int target = 9;
 
             int[] result = findTwoSum(nums, target);
 
+//            System.out.println("-------" + twoSum(nums,target));
+
+            int[] r = twoSum(nums,target);
+
             if (result.length == 0) {
                 System.out.println("No pair found that adds up to the target sum.");
             } else {
                 System.out.println("Pair found at indices: " + result[0] + " and " + result[1]);
                 System.out.println("Numbers are: " + nums[result[0]] + " and " + nums[result[1]]);
+            }
+
+
+            System.out.println("-------------");
+            if (r.length == 0) {
+                System.out.println("No pair found that adds up to the target sum.");
+            } else {
+                System.out.println("Pair found at indices: " + r[0] + " and " + r[1]);
+                System.out.println("Numbers are: " + nums[r[0]] + " and " + nums[r[1]]);
             }
         }
     }
