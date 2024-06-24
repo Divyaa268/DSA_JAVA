6f1c0b80506d591a0a166d9f8c20431815f116d0
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-06-24 00:24:22 -0400

    35. Search Insert Position

--- src/src/Arrays/SearchInsert.java
+++ src/src/Arrays/SearchInsert.java
@@ -18,6 +18,25 @@ public class SearchInsert {
             return nums.length;
         }
 
+    public int searchInsertOptimized(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        // If target is not found, left will be the index to insert
+        return left;
+    }
+
         public static void main(String[] args) {
             SearchInsert solution = new SearchInsert();
 
