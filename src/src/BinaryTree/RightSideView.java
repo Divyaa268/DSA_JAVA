80adfdf5c8142e6bcee0c00ac421c93d2eb8f990
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-06-26 18:03:58 -0400

    Right side view of BT

---
+++ src/src/BinaryTree/RightSideView.java
@@ -0,0 +1,60 @@
+package BinaryTree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+
+public class RightSideView
+{
+        public List<Integer> rightSideView(TreeNode root) {
+            List<Integer> result = new ArrayList<>();
+            if (root == null) {
+                return result;
+            }
+
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+
+            while (!queue.isEmpty()) {
+                int levelSize = queue.size();
+                for (int i = 0; i < levelSize; i++) {
+                    TreeNode currentNode = queue.poll();
+                    if (i == levelSize - 1) {
+                        result.add(currentNode.val);
+                    }
+                    if (currentNode.left != null) {
+                        queue.add(currentNode.left);
+                    }
+                    if (currentNode.right != null) {
+                        queue.add(currentNode.right);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static void main(String[] args) {
+            RightSideView solution = new RightSideView();
+
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(3);
+            root.left.right = new TreeNode(5);
+            root.right.right = new TreeNode(4);
+
+            System.out.println(solution.rightSideView(root)); // Expected output: [1, 3, 4]
+        }
+    }
