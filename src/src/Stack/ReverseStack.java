07ab23e37e8eebbea2faabcdab10277cc20775f1
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2024-07-27 14:51:52 -0400

    reverse stack

--- src/src/Stack/ReverseStack.java
+++ src/src/Stack/ReverseStack.java
@@ -27,22 +27,41 @@ public class ReverseStack {
         pushBottom(s, top);
     }
 
+//    extra memory
+    public static void revusing2Stacks(Stack<Integer> s)
+    {
+        Stack<Integer> st = new Stack<>();
+
+        while(!s.isEmpty())
+        {
+            st.push(s.pop());
+        }
+
+        System.out.println("after rev---");
+
+        while (!st.isEmpty())
+        {
+            System.out.println(st.peek());
+            st.pop();
+        }
+    }
     public static void main(String args[]) {
         Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
-
+//
 //        while (!s.isEmpty()) {
 //            System.out.println(s.peek());
 //            s.pop();
 //        }
 
-        reverseStack(s);
-
-        while (!s.isEmpty()) {
-            System.out.println(s.peek());
-            s.pop();
-        }
+//        reverseStack(s);
+        revusing2Stacks(s);
+//
+//        while (!s.isEmpty()) {
+//            System.out.println(s.peek());
+//            s.pop();
+//        }
     }
 }
